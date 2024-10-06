06e6488b3f9060e8218868baeeecae01afda82ee
Robert Zunt <robert@zunt.net>
2024-10-06 21:28:10 +0200

    Additional description

--- paty_dotaz.sql
+++ paty_dotaz.sql
@@ -1,19 +1,21 @@
+-- Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
+-- Z dat nemůžeme jednoznačně říct, že změna HDP má vliv na změnu potravnin a mezd. Např. v roce 2007 můžeme vidět nárůst ve všech 3 kategoriríh, ale v roce 2015 už tento vzorec neplatí. 	
 WITH HDPzmena AS (
     SELECT 
         e.`year` AS rok,
-        e.GDP AS gdp_value,
-        LAG(e.GDP) OVER (ORDER BY e.`year`) AS previous_gdp,
-        ROUND(((e.GDP - LAG(e.GDP) OVER (ORDER BY e.`year`)) / LAG(e.GDP) OVER (ORDER BY e.`year`)) * 100, 2) AS gdp_change_percent
+        e.GDP AS gdp_hodnota,
+        LAG(e.GDP) OVER (ORDER BY e.`year`) AS predchozi_gdp,
+        ROUND(((e.GDP - LAG(e.GDP) OVER (ORDER BY e.`year`)) / LAG(e.GDP) OVER (ORDER BY e.`year`)) * 100, 2) AS gdp_zmena_procenta
     FROM 
         economies e
     WHERE 
         e.country = 'Czech Republic'
-),
+), -- zde jsem získal informaci o HDP pro ČR
 PlatyAMzdy AS (
     SELECT 
         trzpspf.rok,
-        AVG(trzpspf.prumerna_mzda) AS prumerna_mzda,
-        AVG(trzpspf.prumerna_cena) AS prumerna_cena,
+        round(AVG(trzpspf.prumerna_mzda)) AS prumerna_mzda,
+        round( AVG(trzpspf.prumerna_cena)) AS prumerna_cena,
         LAG(AVG(trzpspf.prumerna_mzda)) OVER (ORDER BY trzpspf.rok) AS predochozi_prum_mzda,
         LAG(AVG(trzpspf.prumerna_cena)) OVER (ORDER BY trzpspf.rok) AS predchozi_prum_cena,
         ROUND(((AVG(trzpspf.prumerna_mzda) - LAG(AVG(trzpspf.prumerna_mzda)) OVER (ORDER BY trzpspf.rok)) / LAG(AVG(trzpspf.prumerna_mzda)) OVER (ORDER BY trzpspf.rok)) * 100, 2) AS mzda_zmena_procenta,
@@ -25,11 +27,11 @@ PlatyAMzdy AS (
 )
 SELECT 
     p.rok,
-    h.gdp_value,
-    h.gdp_change_percent,
+    h.gdp_hodnota,
     p.prumerna_mzda,
-    p.mzda_zmena_procenta,
     p.prumerna_cena,
+    h.gdp_zmena_procenta,
+    p.mzda_zmena_procenta,
     p.cena_zmena_procenta
 FROM 
     HDPzmena h
